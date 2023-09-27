83e6c9a59cb1de27a7c1a9314fef66d3f8f395f0
Codename-Duzers <dedris08@gmail.com>
2023-09-27 22:50:42 +0100

    Update1
    Following things were changed:
    -GameBoard labelling changed--> dictionary keys changed~
    
    -Theory of row checking worked out, however application is not yet complete

--- algorithm.py
+++ algorithm.py
@@ -1,26 +1,38 @@
 WinTrigger = 0
-PieceTypeTrigger = True
 Piece = 0
 
-Dict = {'1': [0,0], '2': [0,1], '3': [0,2], '4': [1,0], '5': [1,1], '6': [1,2], '7': [2,0], '8': [2,1], '9': [2,2]}
+Dict = {'A1': [0,0], 'B1': [0,1], 'C1': [0,2], 'A2': [1,0], 'B2': [1,1], 'C2': [1,2], 'A3': [2,0], 'B3': [2,1], 'C3': [2,2]}
 
-GameBoard = [[1, 2, 3],
-             [4, 5, 6],
-             [7, 8, 9]]
+GameBoard = [['A1', 'B1', 'C1'],
+             ['A2', 'B2', 'C2'],
+             ['A3', 'B3', 'C3']]
 
 PlayedMoves = []
 
-Piece = int(input("Who is the starting player? (Input 1 or 0)"))
+Piece = int(input('Who is the starting player? (Input 1 or 0)'))
 
+def winChecker(board):
+    win = False
+    rowEval = 0
+    for x in board[0]:
+        r1 = 0
+        if board[0][r1] == 1:
+            rowEval = rowEval + 1
+            r1 = r1 + 1 
+        if rowEval == 3:
+            win = True
+            break
+    print(r1)
+    return win
+        
 while WinTrigger == 0:
-    raw_move = input("What position would you like to play?")
+    raw_move = input('What position would you like to play?')
     
-    if int(raw_move[0]) < 1 or int(raw_move[0]) > 9:
+    if raw_move in Dict.items() == False:
         print("This value is outside the scope of the board")
         continue     
     elif raw_move in PlayedMoves:
         print("Sorry, this move has alredy been played, please select another spot")
-        PlayedMoves.append(raw_move)
         continue
     else:
         proc_move = Dict.get(raw_move)
@@ -28,4 +40,5 @@ while WinTrigger == 0:
         PlayedMoves.append(raw_move)
         Piece = Piece * -1 + 1
     
+    print(winChecker(GameBoard))
     print(GameBoard)
